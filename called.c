b7ca3c03a096b6e2eb79aba19174522cb121277e
unknown <gabriele.sau2598@gmail.com>
2022-12-05 20:40:04 +0100

    Second commit

--- called.c
+++ called.c
@@ -5,16 +5,5 @@
 
 int main(int args,char  **kargs)
 {
-    FILE *file;
-    file = fopen(kargs[2],"w");
-
-    if(file!=NULL)
-    {
-        fprintf(file,"%s\n",kargs[1]);
-    }
-    else
-    {
-        printf("Error opening the file!");
-    }
-    return 0;
+    printf("[caller] %s",kargs[1]);
 }
--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 
 int main(){
-
+    int fd;
     char string_1[100], string_2[100];
 
     printf("Inserisci la prima stringa:\n");
@@ -13,11 +14,24 @@ int main(){
     scanf("%c",string_2);
     strcat(string_2,".txt");
 
-    if((execl("./called","called",string_1,string_2,(char *) NULL))==-1)
+    fd = open(string_2, O_WRONLY|O_CREAT|O_APPEND,0644);
+
+    if(fd==-1)
     {
         perror("Error:");
         exit(-1);
     }
+    if(fd==0)
+    {
+        dup2(fd,1);
+        if((execl("./called","called",string_1,(char *) NULL))==-1)
+        {
+            perror("Error:");
+            exit(-1);
+        }
+    }
+
+
 
     return 0;
 }
