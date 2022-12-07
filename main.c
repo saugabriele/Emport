4fd76753ba45da02211f0f1443cf1142fbac4ace
unknown <gabriele.sau2598@gmail.com>
2022-12-07 19:53:10 +0100

    Commit

--- main.c
+++ main.c
@@ -3,42 +3,116 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#define READ 0
+#define WRITE 1
 
 
-int main(){
+int main() {
     int pid;
-    char string_1[100], string_2[100];
+    double fact, number;
+    bool go_ahead;
+    int numpipe[2], controlpipe[2];
+    char ans[1];
+    FILE *file;
 
-    printf("Inserisci la prima stringa:\n");
-    scanf("%['\n']",string_1);
-    printf("Inserisci la seconda stringa(file name):\n");
-    scanf("%c",string_2);
-    strcat(string_2,".txt");
+    if (pipe(numpipe) == -1)
+    {
+        perror("Eror opening pipe");
+        exit(-1);
+    }
 
-    if((pid=fork()) == -1)
+    if (pipe(controlpipe) == -1)
+    {
+        perror("Eror opening pipe");
+        exit(-1);
+    }
+
+    if((pid = fork()) == -1)
     {
         perror("Error:");
         exit(-1);
     }
 
-    switch (pid) {
+    switch(pid)
+    {
         case 0:
-            printf("First child PID: %d\n",getpid(),getppid());
-            if((pid=fork()) == -1)
+            close(controlpipe[WRITE]);
+            close(numpipe[WRITE]);
+
+            while(true)
             {
-                perror("Error:");
-                exit(-1);
-            }
-            switch(pid){
-                case 0:
-                    printf("Second child PID: %d  %d\n",getpid(),getppid());
+                if(read(controlpipe[READ],&go_ahead,sizeof(go_ahead))==-1)
+                {
+                    perror("Error reading: ");
+                    return (-1);
+                }
+                if(!go_ahead)
+                {
                     exit(0);
+                }
+
+                if(read(numpipe[READ], &number, sizeof(number))==-1)
+                {
+                    perror("Error reading: ");
+                    return(-1);
+                }
+                fact = number;
+                while(number >= 2)
+                {
+                    fact *= (number - 1);
+                    number--;
+                }
+                if(fprintf(file, "%f = %f\n",number,fact)==-1)
+                {
+                    perror("Error writing file");
+                    return(-1);
+                }
             }
-            exit(0);
+
 
         default:
-            printf("Parent PID: %d\n",getpid());
-    }
+            close(controlpipe[READ]);
+            close(numpipe[READ]);
 
-    return 0;
+            printf("Do you want to calculate a factorial? [y/n]\n");
+            scanf("%c", ans);
+
+            if(strcmp(ans,"n") == 0)
+            {
+                go_ahead = false;
+                if(write(controlpipe[WRITE],&go_ahead,sizeof(go_ahead))==-1)
+                {
+                    perror("Error:");
+                    exit(-1);
+                }
+                printf("Bye");
+                exit 0;
+            }
+            else if(strcmp(ans,"y") == 0)
+            {
+                go_ahead(true);
+                if(write(controlpipe[WRITE],&go_ahead,sizeof(go_ahead))==-1)
+                {
+                    perror("Error:");
+                    exit(-1);
+                }
+
+                printf("Type the number: \n");
+                scanf("%lf", &number);
+
+                if(write(numpipe[WRITE],&number,sizeof(number))==-1) {
+                    perror("Error: ");
+                    exit(-1);
+                }
+            }
+            else
+            {
+                printf("What?\n");
+            }
+            while(getchar()!= "\n");
+            wait(NULL);
+    }
 }
